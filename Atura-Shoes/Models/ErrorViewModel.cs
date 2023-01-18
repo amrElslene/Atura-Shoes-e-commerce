b96c22e65688e4379a79c7ddaff00e1c128516bf
amrElslene <72973413+amrElslene@users.noreply.github.com>
2023-01-18 20:36:50 +0200

    Add project files.

---
+++ Atura-Shoes/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Atura_Shoes.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
